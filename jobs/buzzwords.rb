720ee399470a7b4f6529f24c84806156f93e8239
bmitchell <bmitchell@blurb.com>
2015-10-16 14:09:03 -0700

    styling

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -2,8 +2,12 @@ buzzwords = ['Paradigm shift', 'Leverage', 'Pivoting', 'Turn-key', 'Streamlinine
 buzzword_counts = Hash.new({ value: 0 })
 
 SCHEDULER.every '2s' do
-  random_buzzword = buzzwords.sample
-  buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
-  
-  send_event('buzzwords', { items: buzzword_counts.values })
+  # random_buzzword = buzzwords.sample
+  # buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+  #
+  # send_event('buzzwords', { items: buzzword_counts.values })
 end
+
+
+
+[{:label=>"Pivoting", :value=>1}]
